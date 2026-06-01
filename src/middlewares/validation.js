ac0e2a86b064330f395a0d81df2c18711944f046
ctrixcode <coderck@proton.me>
2026-06-01 14:19:52 +0530

    fix: dynamically resolve Joi validation options and update request with sanitized values

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -13,13 +13,15 @@ const options = {
 
 module.exports = (schema) => (req, res, next) => {
     Object.keys(schema).forEach((key) => {
-        const { error } = schema[key].validate(req[key], options);
+        const validationOptions = Array.isArray(req[key]) ? options.array : options.basic;
+        const { value, error } = schema[key].validate(req[key], validationOptions);
         console.log(error);
         if (error) {
             console.log(error);
             const message = error.details[0].message || 'Invalid Inputs';
             throw new PreconditionException(message);
         }
+        req[key] = value;
     });
     next();
 };
