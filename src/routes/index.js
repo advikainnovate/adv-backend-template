86ce67f1a34d75118928c413ef14f7b1bed6a716
ctrixcode <coderck@proton.me>
2026-06-01 13:55:21 +0530

    refactor: remove auth module, otp schemas, and endpoint-specific Bruno collections for blank template setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const authRoutes = require('../modules/auth/auth.routes');
 const userRoutes = require('../modules/users/user.routes');
 
-router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
--- src/utils/generate.utils.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-exports.generateStaticOtp = () => {
-    return '112211';
-};
-
-exports.generateRandomOtp = () => {
-    return crypto.randomInt(100000, 999999).toString();
-};
